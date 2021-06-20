ffa00656b9bdb0e50a4d9c7482317dd577403eda
vishnuvs1993 <vishnuvs8476@gmail.com>
2021-06-20 18:03:54 +0530

    version15

--- dproject1/settings.py
+++ dproject1/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'dproject1.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR/'Dapp/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
