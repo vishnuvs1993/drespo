db0ee9b871b7119268d395e2e543215cc6f20690
vishnuvs1993 <vishnuvs8476@gmail.com>
2021-06-23 10:20:12 +0530

    update23

--- Dapp/urls.py
+++ Dapp/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [
-    path('print',views.hello),
-    path('vishnu',views.hello1, name='vishnu')
-   
+    # path('print',views.hello),
+    # path('vishnu',views.hello1, name='vishnu')
+   path('fun1',views.fun)
 ]
